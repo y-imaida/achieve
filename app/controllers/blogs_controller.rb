a995c6789549cf3c9414d7ccf6b7d124c23a5258
Yoshihiro Imaida <yimaida@example.com>
2017-05-15 20:35:06 +0900

    DIVE15 コメント機能を追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,13 @@
 class BlogsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_blog , only: [:edit, :update, :destroy]
+  before_action :set_blog , only: [:show, :edit, :update, :destroy]
   def index
     @blogs = Blog.all
+  end
+
+  def show
+    @comment = @blog.comments.build
+    @comments = @blog.comments
   end
 
   def new
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,46 @@
+class CommentsController < ApplicationController
+  before_action :set_comment_and_blog, only: [:edit, :update, :destroy]
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @blog = @comment.blog
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。'}
+        format.js { render :index }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to blog_path(@blog), notice: 'コメントを編集しました。'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to blog_path(@blog), notice: 'コメントを削除しました。' }
+      format.js { render :index }
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:blog_id, :content)
+    end
+
+    def set_comment_and_blog
+      @comment = Comment.find(params[:id])
+      @blog = @comment.blog
+    end
+end
